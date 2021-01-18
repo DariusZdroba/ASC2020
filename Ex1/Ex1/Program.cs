a5a6d50aa7be70292599fdcdf0427821212ca446
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-18 17:23:02 +0200

    ex

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace pool1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 0.0f;
+            float b = 0.0f;
+            Console.WriteLine("Dati doua numere pentru a forma o ecuatie de gradul I de forma ax+b");
+            Console.WriteLine();
+            Console.WriteLine("a= ");
+            string line = Console.ReadLine();
+            a = float.Parse(line);
+            Console.WriteLine("b=");
+            string line2 = Console.ReadLine();
+            b = float.Parse(line2);
+            Console.WriteLine("Rezultatul ecuatiei este ");
+            b = 0 - b;
+            float s = b / a;
+            Console.Write(s);
+
+
+        }
+    }
+}
