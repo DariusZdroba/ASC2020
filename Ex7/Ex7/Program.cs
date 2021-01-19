8f20380669b1f1ba96e017948fe63d56403bf6b0
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-19 16:51:48 +0200

    ex7

---
+++ Ex7/Ex7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b; string line;
+            line = Console.ReadLine();
+            a = int.Parse(line);
+
+            line = Console.ReadLine();
+            b = int.Parse(line);
+            int c = b;
+            b = a;
+            a = c;
+            Console.WriteLine(a + " " + b);
+            
+
+        }
+    }
+}
