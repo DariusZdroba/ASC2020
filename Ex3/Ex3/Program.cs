117dd31d64a25db55f58ec1eb0dd1f943b3ffda4
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-19 16:24:40 +0200

    Ex3

---
+++ Ex3/Ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k;   string line;
+            Console.WriteLine("Dati 2 numere : ");
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            line = Console.ReadLine();
+            k = int.Parse(line);
+            if (n % k == 0) Console.WriteLine("Se divide ");
+            else Console.WriteLine("Nu se divide");
+        }
+    }
+}
