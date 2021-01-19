a633dcd35d8472cc43192c75a3c0e26380dae55d
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-19 16:27:57 +0200

    ex4

---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n ; string line;
+            Console.WriteLine("Dati un an: ");
+            line = Console.ReadLine();
+            n = int.Parse(line);
+           
+            if (n % 4 == 0) Console.WriteLine("an bisect ");
+            else Console.WriteLine("nu e an bisect");
+        }
+    }
+}
