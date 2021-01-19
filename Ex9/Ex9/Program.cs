f013d78a4bebd8a6af881be274f340780c335df6
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-19 16:57:46 +0200

    ex9

---
+++ Ex9/Ex9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            for (int i = 2; i < n / 2; i++)
+                if (n % i == 0) Console.WriteLine(i + " ");
+
+
+        }
+    }
+}
