76abdf9ab76b62eb886b53c71ae598e879680f1a
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-19 16:43:04 +0200

    ex5

---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line; int n, k, c=0;
+            Console.WriteLine("Dati un numar n ");
+
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            Console.WriteLine("Dati un numar al catelea sa fie extras");
+            line = Console.ReadLine();
+            k = int.Parse(line);
+
+            for (int i = 0; i < k; i++) {
+                if (n > 9)
+                {
+                    c = n % 10;
+                    n = n / 10;
+                }
+                else {
+                    Console.WriteLine("Numarul nu are " + k + " cifre");
+                    break;
+                };
+            }
+            Console.WriteLine("a " + k + " cifra este ");
+            Console.WriteLine(c);
+
+        }
+    }
+}
