7568bbb68ad60d30860ddac6ce6e4ca8ed7b3295
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-19 17:08:51 +0200

    ex11

---
+++ Ex11/Ex11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            string line;
+            line = Console.ReadLine();
+            a = int.Parse(line);
+            while (a > 0)
+            {
+                Console.WriteLine(a % 10 + " ");
+                a = a / 10;
+
+            }
+
+
+
+        }
+    }
+}
