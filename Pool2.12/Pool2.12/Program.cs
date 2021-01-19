c920b836363724dffa3c392a113d09e7f896b91d
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-19 17:22:12 +0200

    pool2.12

---
+++ Pool2.12/Pool2.12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pool2._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, numar, contor = 0 , contor2 = 0; 
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            for(int i = 1; i <= n; i++)
+            {
+                line = Console.ReadLine();
+                numar = int.Parse(line);
+                if (numar != 0) contor++;
+                if (numar == 0)
+                {
+                    if (contor != 0) contor2++;
+                    contor = 0;
+                }
+                }
+            Console.WriteLine(contor2);
+
+
+        }
+    }
+}
