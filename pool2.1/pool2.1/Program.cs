b049f3af052fc2c0bd370fa6c0bf8c8555c09231
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-19 17:18:25 +0200

    pool2.1

---
+++ pool2.1/pool2.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pool2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, numar;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            for(int i = 1; i <= n; i++)
+            {
+                line = Console.ReadLine();
+                numar = int.Parse(line);
+                if (numar % 2 == 0) Console.WriteLine("numar par ");
+
+            }
+
+
+        }
+    }
+}
