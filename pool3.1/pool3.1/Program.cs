6d9a9712dd5c854b5276db3ebe1fb1c0502b1f32
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-19 17:28:16 +0200

    pool3.1

---
+++ pool3.1/pool3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace pool3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxim, suma = 0 ; 
+           
+            
+            string line;
+            line = Console.ReadLine();
+            maxim = int.Parse(line);
+            int[] v = new int[maxim];
+            for (int i = 0; i < maxim; i++)
+            {
+                line = Console.ReadLine();
+                v[i] = int.Parse(line);
+                suma = suma + v[i];
+            }
+            Console.WriteLine(suma);
+        }
+    }
+}
