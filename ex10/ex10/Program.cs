6e501e94b655b13630f8acf79ea939768f568c0e
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-19 17:06:08 +0200

    ex10

---
+++ ex10/ex10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            int i = 2;
+            bool prim = true;
+            while ((i < n / 2) && (prim))
+            {
+                if (n % i == 0)
+                {
+                    prim = false;
+                 
+                }
+                i++;
+            }
+            if (prim) Console.WriteLine("prim ");
+            else Console.WriteLine("nu e prim");
+        }
+    }
+}
