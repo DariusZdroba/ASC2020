1cb3ae528262b33bf22457b0497c5ade97895e96
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 18:56:18 +0200

    pool2.2

---
+++ Pool2.2/Pool2.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pool2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contorNegativ = 0, contorZero =0 , contorPozitiv = 0;
+            int n;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            for(int i = 1; i <= n; i++)
+            {
+                line = Console.ReadLine();
+                int k = int.Parse(line);
+                if (k < 0) contorNegativ++;
+                else if (k == 0) contorZero++;
+                else contorPozitiv++;
+            }
+            Console.WriteLine(contorNegativ + " negative  " + contorZero + " zero " + contorPozitiv + " pozitive ");
+        }
+    }
+}
