6a8f931cecd380b6b0b9a4a5f56a7948bcd426dd
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 18:40:38 +0200

    ex19

---
+++ ex19/ex19/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b = 0, c =0, contor = 0;
+            string line;
+            line = Console.ReadLine();
+            a = int.Parse(line);
+            b = a % 10;
+            while (a > 9)
+            {
+                a = a / 10;
+                c = a % 10;
+                if (c == b) contor++;
+            }
+            if (contor == 2) Console.WriteLine("da");
+            else Console.WriteLine("nu");
+        }
+    }
+}
