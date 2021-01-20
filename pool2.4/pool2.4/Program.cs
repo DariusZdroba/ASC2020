b20eb243381a1b6852c12c6bd885675ac01bf047
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 19:10:44 +0200

    pool2.5

--- pool2.4/pool2.4/Program.cs
+++ pool2.4/pool2.4/Program.cs
@@ -9,8 +9,11 @@ namespace pool2._4
             int a,n;
             int pozitie = -1;
             string line;
+            Console.WriteLine("dati numarul n care reprezinta cate numere sunt in sir ");
             line = Console.ReadLine();
             n = int.Parse(line);
+          
+            Console.WriteLine("dati numarul A care va fi vericiat pe ce pozitie este in sir");
             line = Console.ReadLine();
             a = int.Parse(line);
             for (int i = 0; i < n; i++)
---
+++ pool2.5/pool2.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace pool2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int Contorpozitie = 0;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+          
+            for (int i = 0; i < n; i++)
+            {
+                line = Console.ReadLine();
+                int k = int.Parse(line);
+                if (k == i) Contorpozitie ++;
+
+            }
+            Console.WriteLine(Contorpozitie + " numere ");
+
+        }
+    }
+}
