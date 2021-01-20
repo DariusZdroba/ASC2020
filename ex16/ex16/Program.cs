d35974b9722adc3472a5fb13f8e9d9cf439419c0
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 18:30:42 +0200

    ex18

---
+++ ex16/ex16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e;
+            string line;
+            line = Console.ReadLine();
+            a = int.Parse(line);
+            line = Console.ReadLine();
+            b = int.Parse(line);
+            line = Console.ReadLine();
+            c = int.Parse(line);
+            line = Console.ReadLine();
+            d = int.Parse(line);
+
+            line = Console.ReadLine();
+            e = int.Parse(line);
+                      
+        }
+        }
+
+}
---
+++ ex18/ex18/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ex18
+{
+    class Program
+    {
+        static bool prim(int k)
+        {
+
+            for (int d = 2; d <= k / 2; d++)
+            {
+                if (k % d == 0) return false;
+
+
+            }
+            return true;
+
+        }
+        static void Main(string[] args)
+        {
+            int n,contor = 0;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            int i = 2;
+            bool k = true;
+            while (i <= n / 2)
+            {
+                contor = 0;
+                if (k)
+                {
+                    while (n % i == 0) { 
+                        contor++;
+                    n = n / i;
+                }
+                    if(contor>0) Console.WriteLine(i + "^" + contor);
+                }
+                i++;
+                k = prim(i);
+                
+                            
+
+            }
+
+
+        }
+       
+    }
+}
