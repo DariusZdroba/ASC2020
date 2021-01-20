2930a9bf712a7bdb41e39253f4e8688c91cb496f
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 19:58:59 +0200

    pool2.11

---
+++ pool2.11/pool2.11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace pool2._11
+{
+    class Program
+    {
+       static int invers(int k)
+        {
+            int invers = 0;
+            while (k > 0)
+            {
+                invers = invers * 10 + k % 10;
+                k = k / 10;
+                }
+            return invers;
+
+        }
+        static void Main(string[] args)
+        {
+            string line;
+            int n,k,suma=0;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            for (int i = 1; i <= n; i++) {
+                line = Console.ReadLine();
+                k = int.Parse(line);
+
+                suma = suma + invers(k);
+                    
+                    
+                    }
+
+            Console.WriteLine(suma);
+
+
+
+        }
+    }
+}
