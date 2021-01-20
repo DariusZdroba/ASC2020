c5bdfc002fbf741e6decd7f7b6d36be353d79e3a
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 19:15:48 +0200

    bool2.6

---
+++ pool2.6/pool2.6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace pool2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n ,k ,aux ;
+            bool cresc = true;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            line = Console.ReadLine();
+            aux = int.Parse(line);
+            for (int i = 0; i < n-1; i++)
+            {
+                line = Console.ReadLine();
+                k = int.Parse(line);
+                if(k<aux)
+                {
+                    Console.WriteLine("Nu sunt in ordine");
+                        cresc = false;
+                        break;
+
+                }
+                aux = k;
+
+            }
+            if(cresc)
+            Console.WriteLine("crescator");
+
+        }
+    }
+}
