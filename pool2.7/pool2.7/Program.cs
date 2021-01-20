af897c4101cb30340d4eb1a61116de78ae3350e2
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 19:21:26 +0200

    pool2.7

---
+++ pool2.7/pool2.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pool2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,k;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            line = Console.ReadLine();
+            k = int.Parse(line);
+            int maxim = k;
+            int minim = k;
+            for ( int i = 0; i < n-1; i++)
+            {
+                line = Console.ReadLine();
+                 k = int.Parse(line);
+                if (k > maxim) maxim = k;
+                if (minim > k) minim = k;
+            }
+
+            Console.WriteLine("minimul = " + minim + " maximul = " + maxim);
+        }
+    }
+}
