784423103a31b6d9ebba2290b5cec77511d3e4d9
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 19:05:02 +0200

    pool2.4

---
+++ pool2.4/pool2.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace pool2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,n;
+            int pozitie = -1;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            line = Console.ReadLine();
+            a = int.Parse(line);
+            for (int i = 0; i < n; i++)
+            {
+                line = Console.ReadLine();
+                int k = int.Parse(line);
+                if (k == a) pozitie = i;
+
+            }
+            Console.WriteLine("pozitia " + pozitie);
+        }
+    }
+}
