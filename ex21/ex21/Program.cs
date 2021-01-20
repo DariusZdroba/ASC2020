abd9114569244ef0b8433a9686b3fccff9981df5
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 18:52:28 +0200

    ex21

---
+++ ex21/ex21/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int a;
+            line = Console.ReadLine();
+            a = int.Parse(line);
+            int k = 512;
+               if ((a >= 1) && (a <= 1024))
+            {
+                Console.WriteLine("Daca numarul ales este mai mare decat " + k + "apasati 1");
+                Console.WriteLine("Altfel apasati 0");
+                line = Console.ReadLine();
+                if(line == "1") 
+                
+
+            }
+
+
+        }
+    }
+}
