f0660b670e08bdc482773874080fe728cb829e98
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 20:15:32 +0200

    pool3.3

---
+++ pool3.3/pool3.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pool3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,nrMax,nrMin;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            int[] v = new int[n];
+            line = Console.ReadLine();
+            v[0] = int.Parse(line);
+            nrMax = v[0];
+            nrMin = v[0];
+          
+            for (int i = 1; i < n; i++) {
+                line = Console.ReadLine();
+                v[i] = int.Parse(line);
+                if (v[i] > nrMax) nrMax = v[i];
+                if (v[i] < nrMin) nrMin = v[i];
+            }
+
+            Console.WriteLine("Minim = " + nrMin + " Maxim = " + nrMax);
+
+
+        }
+    }
+}
