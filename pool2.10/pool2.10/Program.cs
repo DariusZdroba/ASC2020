ca5806cb66a21f69a4122ad4adb49846fbb5378a
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 19:55:03 +0200

    pool2.10

---
+++ pool2.10/pool2.10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace pool2._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, contor = 1, contorMax = 1, k;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            line = Console.ReadLine();
+            k = int.Parse(line);
+            int aux = k;
+            for (int i = 1; i < n; i++)
+            {
+                line = Console.ReadLine();
+                k = int.Parse(line);
+                if (aux == k) contor++;
+                else
+                {
+                     if (contor >= contorMax) contorMax = contor;
+                    contor = 1;
+                }
+                aux = k;
+            }
+            Console.WriteLine(contorMax + " numere consecutive");
+
+        }
+    }
+}
