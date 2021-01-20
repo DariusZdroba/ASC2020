3b20737c9981a31610d2ab51945ff66e7817f60f
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 18:58:47 +0200

    pool2.3

---
+++ pool2.3/pool2.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace pool2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0, produs = 1, n;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            for(int i = 1; i <= n; i++)
+            {
+                suma = suma + i;
+                produs = produs * i;
+            }
+            Console.WriteLine("produs = " + produs + " suma = " + suma);
+
+        }
+    }
+}
