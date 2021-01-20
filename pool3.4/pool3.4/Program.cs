5221047883095490f5387d0b7e078db2653169bf
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 23:19:35 +0200

    pool3.4

---
+++ pool3.4/pool3.4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace pool3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, nrMax, nrMin;
+            string line;
+            line = Console.ReadLine();
+            n = int.Parse(line);
+            int[] v = new int[n];
+            line = Console.ReadLine();
+            v[0] = int.Parse(line);
+            nrMax = v[0];
+            nrMin = v[0];
+            int contorMax = 0;
+            int contorMin = 0;
+            for (int i = 1; i < n; i++)
+            {
+                line = Console.ReadLine();
+                v[i] = int.Parse(line);
+                if (v[i] > nrMax)
+                {
+                    nrMax = v[i];
+                    contorMax = 0;
+                }
+                if (v[i] < nrMin)
+                {
+                    nrMin = v[i];
+                    contorMin = 0;
+                }
+                    if (v[i] == nrMax) contorMax++;
+                if (v[i] == nrMin) contorMin++;
+            }
+
+            Console.WriteLine("Minim = " + nrMin +" apare de "+ contorMin + "  Maxim = " + nrMax + " apare de " + contorMax); 
+
+
+        }
+    }
+}
