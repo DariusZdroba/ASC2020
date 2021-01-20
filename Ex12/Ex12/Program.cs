b6e511fdec78ed4fc734cb61bf7d141f37f64880
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 17:55:14 +0200

    ex12

---
+++ Ex12/Ex12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ex12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, n, i;
+            string line;
+            Console.WriteLine("Dati intervalul [a,b]");
+            line = Console.ReadLine();
+            a = int.Parse(line);
+
+            line = Console.ReadLine();
+
+            b = int.Parse(line);
+            Console.WriteLine("Dati numarul n cu care se verifica divizibilitatea n = ");
+            line = Console.ReadLine();
+
+            n = int.Parse(line);
+            if (a < b)
+                for (i = a; i <= b; i++)
+                    if (i % n == 0) Console.WriteLine(i + " ");
+                    else if (b < a)
+                        for ( i = a; i <= b; i++)
+                            if (i % n == 0) Console.WriteLine(i + " ");
+                            else Console.WriteLine("a=b");
+        }
+    }
+}
