449436107dafa91296c6aa8df27a02e3606cecbe
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 18:00:13 +0200

    ex14

---
+++ Ex14/Ex14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ex14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a , c=0;
+            string linge;
+            linge = Console.ReadLine();
+            a = int.Parse(linge);
+            int aux = a;
+            while (a != 0)
+            {
+                c = c * 10 + a % 10;
+                a = a / 10;
+
+            }
+            if (c == aux) Console.WriteLine("palindrom");
+            else Console.WriteLine("nu e palindrom");
+        }
+    }
+}
