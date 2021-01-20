2a1a20298adacb997b815b54b2d77fe17669e7e1
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 18:12:11 +0200

    ex15

---
+++ Ex15/Ex15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            string line;
+            line = Console.ReadLine();
+            a = int.Parse(line);
+            line = Console.ReadLine();
+            b = int.Parse(line);
+            line = Console.ReadLine();
+            c = int.Parse(line);
+
+          /*  if (a > b)
+            { if (b >= c) Console.WriteLine(a + " " + b + " "+ c);
+               if(b<c)
+                { if (a >= c) Console.WriteLine(a + " " + c + " " + b);
+                    if(a<c) Console.WriteLine(c + " " + a + " " + b);
+                } 
+            }
+        */
+            if((a>b)&&(a>c)) if(b>c) Console.WriteLine(c + " " + b + " " + a);
+            else Console.WriteLine(b + " " + c + " " + a);
+
+            if ((b > a) && (b > c)) if (a > c) Console.WriteLine(c + " " + a + " " + b);
+                else Console.WriteLine(a + " " + c + " " + b);
+            if ((c > b) && (c > a)) if (b > a) Console.WriteLine(a + " " + b + " " + c);
+                else Console.WriteLine(b + " " + a + " " + c);
+        }
+    }
+}
