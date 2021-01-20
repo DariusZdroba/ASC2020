67b243cc7d029b950b39e13a292319da9a3f73f3
Darius <Darius@DESKTOP-Q5JFDNI>
2021-01-20 17:57:20 +0200

    ex13

---
+++ Ex13/Ex13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ex12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, n, i;
+            string line;
+            Console.WriteLine("Dati intervalul de ani");
+            line = Console.ReadLine();
+            a = int.Parse(line);
+
+            line = Console.ReadLine();
+
+            b = int.Parse(line);
+           
+            if (a < b)
+                for (i = a; i <= b; i++)
+                    if (i % 4 == 0) Console.WriteLine(i + " ");
+                    else if (b < a)
+                        for (i = a; i <= b; i++)
+                            if (i % 4 == 0) Console.WriteLine(i + " ");
+                            else Console.WriteLine("an1=an2");
+        }
+    }
+}
